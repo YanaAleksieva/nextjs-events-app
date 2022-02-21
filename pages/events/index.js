900db474c3349b407af7d53290ecb3de1f54ec64
YanaAleksieva <ya.aleksieva@gmail.com>
2022-02-21 16:13:27 +0200

    added search filter form

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,17 @@
+import { Fragment } from "react";
+
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/EventsSearch";
 
 const AllEventsPage = () => {
   const events = getAllEvents();
 
   return (
-    <div>
+    <Fragment>
+      <EventsSearch />
       <EventList items={events} />
-    </div>
+    </Fragment>
   );
 };
 
